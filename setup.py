db49907424557f4dde6e6ae029c731b08c510532
Polle Vanhoof <polle@polle-pc.localdomain>
2020-08-22 12:49:13 +0200

    More pypi fixes

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from distutils.core import setup
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
   name = 'python_progress_bar',
   packages = ['python_progress_bar'],
-  version = '1.01',
+  version = '1.02',
   license='MIT',
   description = 'A progress bar for python shell scripts (Linux)',
   author = 'Polle Vanhoof',
   author_email = 'vanhoofpolle@gmail.com',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
+  long_description= 'Please refer to the full description on github',
   url = 'https://github.com/pollev/python_progress_bar',
-  download_url = 'https://github.com/pollev/python_progress_bar/archive/v1.01.tar.gz',
+  download_url = 'https://github.com/pollev/python_progress_bar/archive/v1.02.tar.gz',
   keywords = ['progress', 'bar', 'indicator'],
   install_requires=[
       ],
