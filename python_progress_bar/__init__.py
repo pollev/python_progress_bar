6b8a5bc3b995aca11e2aa7bbf9f6269c14a1a5d5
Polle Vanhoof <polle@polle-pc.localdomain>
2020-08-22 12:30:19 +0200

    Make package pypi compliant

---
+++ python_progress_bar/__init__.py
@@ -0,0 +1,5 @@
+from progress_bar.py import enable_trapping
+from progress_bar.py import setup_scroll_area
+from progress_bar.py import destroy_scroll_area
+from progress_bar.py import draw_progress_bar
+from progress_bar.py import block_progress_bar
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'python_progress_bar',
+  packages = ['python_progress_bar'],
+  version = '1.0',
+  license='MIT',
+  description = 'A progress bar for python shell scripts (Linux)',
+  author = 'Polle Vanhoof',
+  author_email = 'vanhoofpolle@gmail.com',
+  url = 'https://github.com/pollev/python_progress_bar',
+  download_url = '',
+  keywords = ['progress', 'bar', 'indicator'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta', # "3 - Alpha" / "4 - Beta" / "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
